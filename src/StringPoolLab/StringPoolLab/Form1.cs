312e4b43d1690469afa433d760af2dc4d9cb63fe
supershowwei <supershowwei@gmail.com>
2017-03-20 21:56:46 +0800

    complete samples

---
+++ src/StringPoolLab/StringPoolLab/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace StringPoolLab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            this.InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var str1 = this.textBox2.Text;
+            var str2 = string.Intern(this.textBox3.Text);
+            var str3 = "aaa";
+            var str4 = "aaa";
+
+            this.textBox1.Text = string.Concat(
+                $"{str1.Address()}\r\n{str2.Address()}\r\n",
+                $"{str3.Address()}\r\n{str4.Address()}");
+        }
+    }
+}
---
+++ src/StringPoolLab/StringPoolLab/MemoryAddressExtenstion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StringPoolLab
+{
+    public static class MemoryAddressExtenstion
+    {
+        public static string Address(this object me)
+        {
+            unsafe
+            {
+                TypedReference tr = __makeref(me);
+                var ptr = **(IntPtr**)&tr;
+                return "0x" + ptr.ToString("X");
+            }
+        }
+    }
+}
